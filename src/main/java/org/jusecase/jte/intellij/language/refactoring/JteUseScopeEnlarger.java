e2ff60dc0575c78430c689f3f48bca4207f3245f
Andreas Hager <andy@mazebert.com>
2020-06-14 08:32:11 +0200

    Add UseScopeEnhancer, to allow refactoring of tag/template parameters

---
+++ src/main/java/org/jusecase/jte/intellij/language/refactoring/JteUseScopeEnlarger.java
@@ -0,0 +1,25 @@
+package org.jusecase.jte.intellij.language.refactoring;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiParameter;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.SearchScope;
+import com.intellij.psi.search.UseScopeEnlarger;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class JteUseScopeEnlarger extends UseScopeEnlarger {
+    @Nullable
+    @Override
+    public SearchScope getAdditionalUseScope(@NotNull PsiElement element) {
+        if (!(element instanceof PsiParameter)) {
+            return null;
+        }
+
+        if (!element.getContainingFile().getName().endsWith(".jte")) {
+            return null;
+        }
+
+        return GlobalSearchScope.projectScope(element.getProject());
+    }
+}
