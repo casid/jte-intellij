04afdadd9c6c85140399f52310cd3d4c23a7a9e0
Andreas Hager <andy@mazebert.com>
2021-01-07 09:18:33 +0100

    Optimize formatting after Java auto imports

--- src/main/java/org/jusecase/jte/intellij/language/JteJavaContentManipulator.java
+++ src/main/java/org/jusecase/jte/intellij/language/JteJavaContentManipulator.java
@@ -1,14 +1,21 @@
 package org.jusecase.jte.intellij.language;
 
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.AbstractElementManipulator;
 import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jusecase.jte.intellij.language.psi.JtePsiImport;
 import org.jusecase.jte.intellij.language.psi.JtePsiJavaContent;
+import org.jusecase.jte.intellij.language.psi.JtePsiJavaInjection;
+
+import java.util.Collection;
 
 public class JteJavaContentManipulator extends AbstractElementManipulator<JtePsiJavaContent> {
     @Nullable
@@ -20,10 +27,61 @@ public class JteJavaContentManipulator extends AbstractElementManipulator<JtePsi
             if (documentManager.isDocumentBlockedByPsi(document)) {
                 documentManager.doPostponedOperationsAndUnblockDocument(document);
             }
+
             document.replaceString(range.getStartOffset(), range.getEndOffset(), newContent);
             documentManager.commitDocument(document);
+
+            optimizeImportsAfterContentManipulation(document, element, newContent);
         }
 
         return element;
+    }
+
+    private void optimizeImportsAfterContentManipulation(@NotNull Document document, @NotNull JtePsiJavaContent element, String newContent) {
+        if (isRequiredToOptimizeImports(element, newContent)) {
+            ApplicationManager.getApplication().invokeLater(() -> {
+                //noinspection DialogTitleCapitalization
+                WriteCommandAction.runWriteCommandAction(element.getProject(), "Optimize jte Imports", null, () -> {
+                    String optimizedText = document.getText().replace(";import ", "\n@import ");
+                    document.setText(optimizedText);
+                }, element.getContainingFile());
+            });
+        }
+    }
+
+    private boolean isRequiredToOptimizeImports(@NotNull JtePsiJavaContent element, String newContent) {
+        if (!newContent.contains(";import ")) {
+            return false;
+        }
+
+        Collection<JtePsiImport> jtePsiImports = PsiTreeUtil.findChildrenOfType(element, JtePsiImport.class);
+        for (JtePsiImport jtePsiImport : jtePsiImports) {
+            JtePsiJavaInjection javaInjection = PsiTreeUtil.findChildOfType(jtePsiImport, JtePsiJavaInjection.class);
+            if (javaInjection == null) {
+                continue;
+            }
+
+            String text = javaInjection.getText();
+            if (!text.contains(";import ")) {
+                continue;
+            }
+
+            String[] classes = text.split(";import ");
+            if (classes.length < 2) {
+                continue;
+            }
+
+            int startSearchIndex = javaInjection.getTextOffset() + text.length();
+
+            for (int i = 1; i < classes.length; ++i) {
+                if (newContent.indexOf(classes[i], startSearchIndex) != -1) {
+                    return false;
+                }
+            }
+
+            break;
+        }
+
+        return true;
     }
 }
