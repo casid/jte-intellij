b6318456f8f35a97ff063960d4e2877515327a9d
casid <andy@mazebert.com>
2021-01-09 11:25:23 +0100

    Use TypedHandlerDelegate instead of deprecated TypedActionHandlerBase

--- src/main/java/org/jusecase/jte/intellij/language/completion/JteTypedActionHandler.java
+++ src/main/java/org/jusecase/jte/intellij/language/completion/JteTypedActionHandler.java
@@ -1,10 +1,8 @@
 package org.jusecase.jte.intellij.language.completion;
 
 import com.intellij.codeInsight.AutoPopupController;
-import com.intellij.codeInsight.template.impl.editorActions.TypedActionHandlerBase;
-import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.codeInsight.editorActions.TypedHandlerDelegate;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.actionSystem.TypedActionHandler;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiDocumentManager;
@@ -13,36 +11,25 @@ import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 import org.jusecase.jte.intellij.language.parsing.JteTokenTypes;
 
-public class JteTypedActionHandler extends TypedActionHandlerBase {
-
-    public JteTypedActionHandler(TypedActionHandler originalHandler) {
-        super(originalHandler);
-    }
+public class JteTypedActionHandler extends TypedHandlerDelegate {
 
     @Override
-    public void execute(@NotNull Editor editor, char charTyped, @NotNull DataContext dataContext) {
-        if (myOriginalHandler != null) {
-            myOriginalHandler.execute(editor, charTyped, dataContext);
-        }
-
-        // Must be invoked after original handler, otherwise android xml handler overwrites the auto popup after we set it...
+    public @NotNull Result charTyped(char charTyped, @NotNull Project project, @NotNull Editor editor, @NotNull PsiFile file) {
         if (charTyped == '@' || charTyped == '$' || charTyped == '!') {
-            Project project = editor.getProject();
-            if (project == null) {
-                return;
-            }
-
             PsiFile psiFile = PsiDocumentManager.getInstance(project).getPsiFile(editor.getDocument());
             if (psiFile == null) {
-                return;
+                return Result.CONTINUE;
             }
 
             if (psiFile.getFileElementType() != JteTokenTypes.FILE) {
-                return;
+                return Result.CONTINUE;
             }
 
             invokeAutoPopup(project, editor, charTyped);
+            return Result.STOP;
         }
+
+        return Result.CONTINUE;
     }
 
     private void invokeAutoPopup(@NotNull Project project, @NotNull Editor editor, char charTyped) {
