516bd661352b0d88936b407a975eed9c89fecd01
Andreas Hager <andy@mazebert.com>
2021-01-15 05:42:53 +0100

    Import formatting works, even if there's still an absolute usage of an imported class

--- src/main/java/org/jusecase/jte/intellij/language/JteJavaContentManipulator.java
+++ src/main/java/org/jusecase/jte/intellij/language/JteJavaContentManipulator.java
@@ -7,15 +7,10 @@ import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.AbstractElementManipulator;
 import com.intellij.psi.PsiDocumentManager;
-import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jusecase.jte.intellij.language.psi.JtePsiImport;
 import org.jusecase.jte.intellij.language.psi.JtePsiJavaContent;
-import org.jusecase.jte.intellij.language.psi.JtePsiJavaInjection;
-
-import java.util.Collection;
 
 public class JteJavaContentManipulator extends AbstractElementManipulator<JtePsiJavaContent> {
     @Nullable
@@ -38,7 +33,7 @@ public class JteJavaContentManipulator extends AbstractElementManipulator<JtePsi
     }
 
     private void optimizeImportsAfterContentManipulation(@NotNull Document document, @NotNull JtePsiJavaContent element, String newContent) {
-        if (isRequiredToOptimizeImports(element, newContent)) {
+        if (isRequiredToOptimizeImports(newContent)) {
             ApplicationManager.getApplication().invokeLater(() -> {
                 //noinspection DialogTitleCapitalization
                 WriteCommandAction.runWriteCommandAction(element.getProject(), "Optimize jte Imports", null, () -> {
@@ -49,39 +44,7 @@ public class JteJavaContentManipulator extends AbstractElementManipulator<JtePsi
         }
     }
 
-    private boolean isRequiredToOptimizeImports(@NotNull JtePsiJavaContent element, String newContent) {
-        if (!newContent.contains(";import ")) {
-            return false;
-        }
-
-        Collection<JtePsiImport> jtePsiImports = PsiTreeUtil.findChildrenOfType(element, JtePsiImport.class);
-        for (JtePsiImport jtePsiImport : jtePsiImports) {
-            JtePsiJavaInjection javaInjection = PsiTreeUtil.findChildOfType(jtePsiImport, JtePsiJavaInjection.class);
-            if (javaInjection == null) {
-                continue;
-            }
-
-            String text = javaInjection.getText();
-            if (!text.contains(";import ")) {
-                continue;
-            }
-
-            String[] classes = text.split(";import ");
-            if (classes.length < 2) {
-                continue;
-            }
-
-            int startSearchIndex = javaInjection.getTextOffset() + text.length();
-
-            for (int i = 1; i < classes.length; ++i) {
-                if (newContent.indexOf(classes[i], startSearchIndex) != -1) {
-                    return false;
-                }
-            }
-
-            break;
-        }
-
-        return true;
+    private boolean isRequiredToOptimizeImports(String newContent) {
+        return newContent.contains(";import ");
     }
 }
