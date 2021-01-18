8214c649c0341c65a91a73e25d93d9006f125ba3
Andreas Hager <andy@mazebert.com>
2021-01-18 08:18:22 +0100

    Correct indentation for param/import, when pressing enter

--- src/main/java/org/jusecase/jte/intellij/language/format/JteFormattingBlock.java
+++ src/main/java/org/jusecase/jte/intellij/language/format/JteFormattingBlock.java
@@ -3,12 +3,16 @@ package org.jusecase.jte.intellij.language.format;
 import com.intellij.formatting.*;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
+import com.intellij.psi.formatter.common.AbstractBlock;
 import com.intellij.psi.formatter.xml.XmlFormattingPolicy;
+import com.intellij.psi.tree.IElementType;
 import com.intellij.xml.template.formatter.AbstractXmlTemplateFormattingModelBuilder;
 import com.intellij.xml.template.formatter.TemplateLanguageBlock;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jusecase.jte.intellij.language.parsing.JteTokenTypes;
+
+import java.util.List;
 
 public class JteFormattingBlock extends TemplateLanguageBlock {
     protected JteFormattingBlock(AbstractXmlTemplateFormattingModelBuilder builder, @NotNull ASTNode node, @Nullable Wrap wrap, @Nullable Alignment alignment, CodeStyleSettings settings, XmlFormattingPolicy xmlFormattingPolicy, @Nullable Indent indent) {
@@ -31,5 +35,33 @@ public class JteFormattingBlock extends TemplateLanguageBlock {
     @Override
     public @Nullable Spacing getSpacing(@Nullable Block child1, @NotNull Block child2) {
         return null;
+    }
+
+    @Override
+    public @NotNull ChildAttributes getChildAttributes(int newChildIndex) {
+        AbstractBlock previousBlock = getPreviousBlock(newChildIndex);
+
+        if (previousBlock != null) {
+            IElementType elementType = previousBlock.getNode().getElementType();
+            if (elementType == JteTokenTypes.IMPORT || elementType == JteTokenTypes.PARAM) {
+                return new ChildAttributes(Indent.getNoneIndent(), null);
+            }
+        }
+
+        return super.getChildAttributes(newChildIndex);
+    }
+
+    private AbstractBlock getPreviousBlock(int newChildIndex) {
+        List<Block> subBlocks = getSubBlocks();
+        if (newChildIndex > subBlocks.size()) {
+            return null;
+        }
+
+        Block previousBlock = subBlocks.get(newChildIndex - 1);
+        if (!(previousBlock instanceof AbstractBlock)) {
+            return null;
+        }
+
+        return (AbstractBlock)previousBlock;
     }
 }
