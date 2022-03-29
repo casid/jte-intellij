ecd688ebc118d05f4e9c673efa9af9c8ae3de718
Andreas Hager <andy@mazebert.com>
2022-03-29 05:18:35 +0200

    Fix index out of bounds exception

--- src/main/java/org/jusecase/jte/intellij/language/parsing/parsers/AbstractTokenParser.java
+++ src/main/java/org/jusecase/jte/intellij/language/parsing/parsers/AbstractTokenParser.java
@@ -13,7 +13,7 @@ public abstract class AbstractTokenParser extends TokenParser {
     }
 
     protected final boolean isBeginOf(int position, char token) {
-        if (position > myEndOffset) {
+        if (position >= myEndOffset) {
             return false;
         }
         return myBuffer.charAt(position) == token;
