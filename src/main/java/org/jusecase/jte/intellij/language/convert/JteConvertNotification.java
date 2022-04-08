967861cccc69b7eb3734e9a030e7b3d4d47e573d
Andreas Hager <andy@mazebert.com>
2022-04-08 05:45:00 +0200

    Resolve NotificationGroup deprecations

--- src/main/java/org/jusecase/jte/intellij/language/convert/JteConvertNotification.java
+++ src/main/java/org/jusecase/jte/intellij/language/convert/JteConvertNotification.java
@@ -4,10 +4,11 @@ import com.intellij.notification.*;
 import com.intellij.openapi.project.Project;
 
 public class JteConvertNotification {
-    private static final NotificationGroup STICKY_GROUP = new NotificationGroup("org.jusecase.jte.intellij.language.convert.notification", NotificationDisplayType.STICKY_BALLOON, true);
+    private static final NotificationGroup STICKY_GROUP = NotificationGroupManager.getInstance().getNotificationGroup("org.jusecase.jte.intellij.language.convert.notification");
 
     public static void error(Project project, String title, String content) {
-        Notification notification = STICKY_GROUP.createNotification(title, null, content, NotificationType.ERROR);
+        Notification notification = STICKY_GROUP.createNotification(content, NotificationType.ERROR);
+        notification.setTitle(title);
         Notifications.Bus.notify(notification, project);
     }
 }
